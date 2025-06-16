bc429b8c223a0c5a158997155bbdaa68ca9ee7f0
Martin <martin.funkquist@gmail.com>
2025-06-16 12:51:44 +0200

    Added grayscale argument to test script

--- src/test.py
+++ src/test.py
@@ -11,10 +11,11 @@ from diffusion_model import UNET, DDPMScheduler
 
 arg_parser = argparse.ArgumentParser(description="Test a saved model")
 arg_parser.add_argument("--checkpoint_path", type=str, required=True, help="Path to the model checkpoint")
-arg_parser.add_argument("--num_time_steps", type=int, default=1000, help="Number of time steps for the diffusion model")
-arg_parser.add_argument("--ema_decay", type=float, default=0.9999, help="Decay rate for the EMA model")
 arg_parser.add_argument("--data_folder", type=str, required=True, help="Directory containing the data")
 arg_parser.add_argument("--output_folder", type=str, default=None, help="Directory to save the output images")
+arg_parser.add_argument("--num_time_steps", type=int, default=1000, help="Number of time steps for the diffusion model")
+arg_parser.add_argument("--ema_decay", type=float, default=0.9999, help="Decay rate for the EMA model")
+arg_parser.add_argument("--grayscale", action='store_true', help="Use grayscale images (default is RGB)")
 args = arg_parser.parse_args()
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu" 
@@ -33,11 +34,12 @@ def inference(checkpoint_path: str,
               data_folder: str,
               num_time_steps: int = 1000,
               ema_decay: float = 0.9999,
-              output_folder: str | None = None):
+              output_folder: str | None = None,
+              grayscale: bool = False):
     if output_folder is not None and not os.path.exists(output_folder):
       os.makedirs(output_folder)
     checkpoint = torch.load(checkpoint_path, map_location=DEVICE)
-    train_dataset = SokobanDataset(data_folder=data_folder)
+    train_dataset = SokobanDataset(data_folder=data_folder, grayscale=grayscale)
     input_channels = train_dataset[0].shape[0]
     input_dimensions = train_dataset[0].shape
 
@@ -87,4 +89,5 @@ inference(args.checkpoint_path,
           args.data_folder,
           num_time_steps=args.num_time_steps,
           ema_decay=args.ema_decay,
-          output_folder=args.output_folder)
+          output_folder=args.output_folder,
+          grayscale=args.grayscale)
